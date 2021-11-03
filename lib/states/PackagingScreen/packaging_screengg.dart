0ba1448334bc3491efaf87b287f5c42f5cc4c959
phatcharaphon23 <phatcharaphon.h@ku.th>
2021-11-03 09:50:30 +0700

    เอา count ออก

--- lib/states/PackagingScreen/packaging_screengg.dart
+++ lib/states/PackagingScreen/packaging_screengg.dart
@@ -25,23 +25,46 @@ class _PackagingScreenState extends State<PackagingScreen> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(),
-                  color: Myconstant.primary),
-            ),
-          ],
-        ),
+      body: const Center(
+        child: MyStatefulWidget(),
       ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'One';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 20,
+      elevation: 10,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
